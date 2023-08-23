94e836828eb1ac97dfcf306b1c7bad41abb15d27
Juan Gabriel Rodriguez <nogren23@gmail.com>
2023-08-23 10:36:42 -0300

    NavBar&Post-Upgrade

--- blog-frontend/src/Components/NavBar/NavBar.jsx
+++ blog-frontend/src/Components/NavBar/NavBar.jsx
@@ -1,42 +1,163 @@
-import React from 'react';
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import HomeIcon from '@mui/icons-material/Home';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
-import {Box,Grid} from '@mui/material'
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import WorkspacesIcon from '@mui/icons-material/Workspaces';
 
-const Navbar = () => {
+const pages = ['Home', 'Categories', 'Profile'];
+const settings = ['Profile', 'Logout'];
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <AppBar >
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="home">
-          <HomeIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Blogger 2 
-        </Typography>
-        <Button color="inherit"  >
-          Home
-        </Button>
-        <Button color="inherit" >
-          Categories
-        </Button>
-        <Button color="inherit" >
-          Posts
-        </Button>
-        <Button color="inherit" >
-          Profile
-        </Button>
-        <IconButton edge="end" color="inherit" aria-label="profile">
-          <AccountCircleIcon />
-        </IconButton>
-      </Toolbar>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <WorkspacesIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            BLOGGO 
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <WorkspacesIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            BLOGGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
-};
-
-export default Navbar;
+}
+export default NavBar;
--- blog-frontend/src/Components/PostContainer/PostContainer.jsx
+++ blog-frontend/src/Components/PostContainer/PostContainer.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@mui/material';
+import Post from '../Post/Post' // Asegúrate de ajustar la ruta al archivo Post.js
+
+const posts = [
+  {
+    id: 1,
+    title: 'Mi primer post',
+    date: 'October 22, 2023 ',
+    content: 'Method:Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes.Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook again without stirring, until mussels have opened and rice is just tender, 5 to 7 minutes more. (Discard any mussels that dont Set aside off of the heat to let rest for 10 minutes, and then serve.',
+    author: 'Usuario123',
+  },
+  {
+    id: 2,
+    title: 'Mi segundo post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  {
+    id: 1,
+    title: 'Mi primer post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  {
+    id: 1,
+    title: 'Mi primer post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  {
+    id: 1,
+    title: 'Mi primer post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  {
+    id: 1,
+    title: 'Mi primer post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  {
+    id: 1,
+    title: 'Mi primer post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  {
+    id: 1,
+    title: 'Mi primer post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  {
+    id: 1,
+    title: 'Mi primer post',
+    content: 'Este es el contenido de mi primer post...',
+    author: 'Usuario123',
+  },
+  // Agrega más objetos de ejemplo aquí
+];
 
 const PostContainer = () => {
   return (
-    <div>PostContainer</div>
-  )
-}
+    <Grid container spacing={3}>
+      {posts.map((post) => (
+        <Grid item key={post.id} xs={12} >
+          <Post
+            title={post.title}
+            date={post.date}
+            content={post.content}
+            author={post.author}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
-export default PostContainer
+export default PostContainer;
